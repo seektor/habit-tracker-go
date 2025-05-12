64277bdaa09bf62b3e4553255c4cc8b74cb4ea9b
Sektor <krzysiek1230@gmail.com>
2025-05-13 00:08:47 +0200

    Print available commands

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -1,30 +1,50 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
-	"log"
+	"os"
+	"strings"
 
 	"github.com/seektor/habit-tracker-go/internal/habit"
+	"github.com/seektor/habit-tracker-go/internal/utils"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(utils.FgColors.FgYellow + utils.FgColors.FgBold + "=== Habit Tracker ===" + utils.FgColors.FgReset)
+	fmt.Println()
+	fmt.Println(getCommandText("p  [index?]", "Print all habits / a habit"))
+	fmt.Println(getCommandText("a  [name] [stepTime] [stepsCount]", "Add a habit"))
+	fmt.Println(getCommandText("d  [index]", "Delete a habit"))
+	fmt.Println(getCommandText("sc [index] [stepTime]", "Change step time of a habit"))
+	fmt.Println(getCommandText("sc [index] [stepsCount]", "Change number of steps"))
+	fmt.Println(getCommandText("f  [index?]", "Freeze all habits / a habit"))
+	fmt.Println(getCommandText("uf [index?]", "Unfreeze all habits / a habi"))
+	fmt.Println(getCommandText("q", "Quit"))
+	fmt.Println()
+
+	fmt.Print("Enter command: ")
+	reader := bufio.NewReader(os.Stdin)
+	// ReadString will block until the delimiter is entered
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occured while reading input. Please try again", err)
+		return
+	}
+
+	// remove the delimeter from the string
+	input = strings.TrimSuffix(input, "\n")
+	commands := strings.Fields(input)
+	if commands[0] == "q" {
+		os.Exit(0)
+	}
+	fmt.Println(input)
 
 	habits := habit.NewHabits()
 
 	habits.Create("Test 1", 1, 60)
-	fmt.Printf("%v", habits.Habits[0].Summary.History)
 }
 
-func printHabits(habits habit.Habits) {
-	fmt.Println("-- Printing Habits --")
-
-	res, err := json.MarshalIndent(habits, "", "  ")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(string(res))
+func getCommandText(command string, description string) string {
+	return fmt.Sprintf("%s%s%s   %s", utils.FgColors.FgBold, command, utils.FgColors.FgReset, description)
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,6 +2,18 @@ package utils
 
 import "time"
 
+var FgColors = struct {
+	FgReset  string
+	FgYellow string
+	FgGreen  string
+	FgBold   string
+}{
+	FgReset:  "\033[0m",
+	FgYellow: "\033[33m",
+	FgGreen:  "\033[32m",
+	FgBold:   "\033[1m",
+}
+
 func getBeginningOfDayDate(t time.Time) time.Time {
 	year, month, day := t.Date()
 	return time.Date(year, month, day, 0, 0, 0, 0, time.UTC)
